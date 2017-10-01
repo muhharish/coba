37fce70dca14bc4ab38511bd20224380feeae40b
muhharish <muhharishlm@gmail.com>
2017-10-01 20:23:33 +0700

    awal

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Extract XII</title> 
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/freelancer.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/scrollReveal.min"></script>
+	
+
+	<!-- Custom Fonts -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header page-scroll">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="glyphicon glyphicon-th-large"></span>
+				</button>
+				<div class="navi">
+					<a class="navbar-brand" href="#"><b>KeDai</b> <span>Computerworks</span></a>
+				</div>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="hidden">
+						<a href="#page-top"></a>
+					</li>
+					<li class="page-scroll">
+						<a href="#Profile"><b>Profile</b></a>
+					</li>
+					<li class="page-scroll">
+						<a href="#Gallery"><b>Galery</b></a>
+					</li>
+					<li class="page-scroll">
+						<a href="#Event"><b>Event</b></a>
+					</li>
+					<li class="page-scroll">
+						<a href="#Contact"><b>Contact</b></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="header" style="height:600px">
+		<div class="container">	
+			<div class="row">
+				<div class="col-xs-12 col-md-12">
+					<a href="#home" class="thumbnail ">
+						<img src="img/11.png" alt="">
+					</a>
+				</div>
+			</div>
+			<div class="intro-text">
+				<span class="name"><b>KeDai</b> <i> Computerworks</i></span>
+				<hr class="star-primary">
+				<span class="skills">Berdiri Sama Tinggi | Duduk Sama Rendah | Ketawa Sama-Sama</span>
+			</div>
+		</div>
+	</div>
+	<div id="Profile">
+		<div class="jumbotron">
+			<div class="container">
+				<div class="thumbnail">
+					<div class="col-md-6 col-xs-12">
+						<img src="img/as.png" >
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-12">
+					<h2>Blue Folder</h2>
+					<p><b>KeDai</b> <span>ComputerWorks</span> adalah salah satu komunitas IT di STMIK Dipanegara Makassar. Organisasi ini mulai muncul pada tahun 1998 dan didirikan oleh sekelompok (14 orang Pelopor) mahasiswa STMIK DIpanegara Makassar yang peduli bahwa wadah seperti inilah yang bisa dijadikan tempat meningkatkan kreatifitas dalam pengembangan Teknologi Informasi.<br>
+						<b>KeDai</b> <span>ComputerWorks</span> resmi menjadi UKM STMIK Dipanegara pada tanggal 11 Desember 2000. KeDai Computerworks merupakan Unit Kegiatan Mahasiswa yang berbasis Study Club yang mempelajari beberapa bidang dalam ICT dan perkembangannya.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="Gallery">
+		<div class="gal">
+			<div class="container">
+				<h2>Galery</h2>
+				<div class="row">
+					<div class="col-xs-12 col-md-4">
+						<a href="#" class="thumbnail">
+							<img src="img/1.png" alt="">
+						</a>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<a href="#" class="thumbnail">
+							<img src="img/1.png" alt="">
+						</a>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<a href="#" class="thumbnail">
+							<img src="img/1.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="Contact">
+		<div class="sosial">
+			<div class="container">
+				<h2>Contact Us</h2>
+				<h3>Around the Web</h3>
+				<ul class="list-inline">
+					<li>
+						<a href="https://www.facebook.com/kdcwstmikdp?fref=ts" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+					</li>
+					<li>
+						<a href="https://twitter.com/" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+					</li>
+					<li>
+						<a href= class="btn-social btn-outline"><i class="fa fa-yahoo""></i></a>
+					</li>
+				</ul>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.775707676031!2d119.50537931435083!3d-5.139776653414191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee354fcbc4e4d%3A0xb28776efd1d9b919!2sKedai+Computerworks!5e0!3m2!1sid!2sid!4v1450940428801" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+			</div>
+		</div>
+	</div>
+
+</div>	
+<div class="footer">
+	Copyright 2015 - <b>KeDai</b> <i>Computerworks</i>
+</div>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>
+<script src="js/freelancer.js"></script>
+<script src="js/cbpAnimatedHeade.min.js"></script>
+
+
+
+</body>
+</html>
---
+++ js/abc.js
@@ -0,0 +1,9 @@
+$(function(){
+      $(".typed").typed({
+        strings: ["di kedai" , "seru","menarik"],
+        typeSpeed: 100,
+        loop: true,
+        backDelay: 1200
+      });
+  });
+
